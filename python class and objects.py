a25b78bcb15719955a7282e639558c9efabe49bd
RAGU_VGLUG <ragu26555@gmail.com>
2024-09-29 11:40:14 -0700

    python class and objects.py

--- python class and objects.py
+++ python class and objects.py
@@ -1,3 +1,4 @@
+#BASIC class and object creation
 class vehicle:
     def tata(brand,color,model):
         print("Model is:",model)
@@ -5,3 +6,39 @@ class vehicle:
         print("Brand is:",brand)
 cars=vehicle
 car_model=cars.tata("tata1761","navy blue","tata")
+
+#PROGRAM FOR ENCAPSULATION AND DATA ABSTRACTION
+class cars:
+    def __init__(self,car):
+            self.car=car
+        
+    def list_cars(self):
+        print("Available cars")
+        for cars_list in self.car:
+            print(cars_list)
+            
+    def borrow_car(self,buying_car):
+        
+        if buying_car in self.car:
+            print("Get your car")
+            self.car.remove(buying_car) 
+        else:
+            print("The car is not Available")
+available_cars=["Maruti Suzuki","Hyundai","Tata Motors","Mahindra","Kia","Toyota","MG Motor","Honda","Renault","Skoda"]
+designs=cars(available_cars)
+presentation="""
+1.Display cars
+2.Borrow car
+"""
+while True:
+    print(presentation)
+    
+    choice=int(input("Enter your choice:"))
+    if choice==1:
+        designs.list_cars()
+    
+    elif choice==2:
+        a=input("Enter the  car name:")
+        designs.borrow_car(a)
+    else:
+        quit()       
