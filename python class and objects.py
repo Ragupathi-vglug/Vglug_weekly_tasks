7886a4be06738dc366f13bc43b392bf9275e5422
RAGU_VGLUG <ragu26555@gmail.com>
2024-09-29 20:06:43 +0530

    Add files via upload

---
+++ python class and objects.py
@@ -0,0 +1,8 @@
+class vehicle:
+    def tata(brand,color,model):
+        print("Model is:",model)
+        print("Color is:",color)
+        print("Model is:",brand)
+cars=vehicle
+car_model=cars.tata("tata1761","navy blue","tata")
+    
