fe149dafeb28b2091d9e65fcca43de7c5ca491d1
RAGU_VGLUG <ragu26555@gmail.com>
2024-09-29 09:53:24 -0700

    Update python class and objects.py

--- python class and objects.py
+++ python class and objects.py
@@ -2,7 +2,6 @@ class vehicle:
     def tata(brand,color,model):
         print("Model is:",model)
         print("Color is:",color)
-        print("Model is:",brand)
+        print("Brand is:",brand)
 cars=vehicle
 car_model=cars.tata("tata1761","navy blue","tata")
-    
